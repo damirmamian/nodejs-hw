f957b7d01d702df9e80938d819e19e85d16061cd
damirmamian <damirmamian@gmail.com>
2026-06-20 13:10:00 +0300

    solved minor issues

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -10,7 +10,7 @@ export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
   if (!note) {
-    return res.status(404).json({ message: 'Student not found' });
+    throw createHttpError(404, 'Note not found');
   }
   res.status(200).json(note);
 };
@@ -26,7 +26,7 @@ export const deleteNote = async (req, res) => {
     _id: noteId,
   });
   if (!note) {
-    throw createHttpError(404, 'Student not found');
+    throw createHttpError(404, 'Note not found');
   }
   res.status(200).json(note);
 };
@@ -41,7 +41,7 @@ export const updateNote = async (req, res) => {
   );
 
   if (!note) {
-    throw createHttpError(404, 'Student not found');
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ import 'dotenv/config';
 import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
-import { notFound } from './middleware/notFoundHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from './routes/notesRoutes.js';
+import notesRouter from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -14,9 +14,9 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
-app.use(notesRoutes);
+app.use(notesRouter);
 
-app.use(notFound);
+app.use(notFoundHandler);
 app.use(errorHandler);
 
 await connectMongoDB();
