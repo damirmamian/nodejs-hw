719966eae460483906172d2d246d6bf1c53d5562
damirmamian <damirmamian@gmail.com>
2026-06-27 12:30:53 +0300

    literally everything was fine

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -3,7 +3,7 @@ import { Joi, Segments } from 'celebrate';
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 };
 
